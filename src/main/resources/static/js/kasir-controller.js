e2e37a68d48e7832c4032d80ae7fa1038ec5ca1a
royryando <royryando7@gmail.com>
2017-06-21 15:24:15 +0700

    web resource

--- src/main/resources/static/js/kasir-controller.js
+++ src/main/resources/static/js/kasir-controller.js
@@ -14,7 +14,9 @@
 		var vm = this;
 		
 		vm.produk = [];
+		vm.deskripsi = [];
 		vm.listProduk = listProduk;
+		vm.deskripsiProduk = deskripsiProduk;
 		
 		init();
 		
@@ -27,6 +29,14 @@
 			var kasirPromise = $http.get(url);
 			kasirPromise.then(function(response){
 				vm.produk = response.data;
+			});
+		}
+		
+		function deskripsiProduk(){
+			var url = "http://localhost:8080/api/produk/list";
+			var kasirPromise = $http.get(url);
+			kasirPromise.then(function(response){
+				vm.deskripsi = response.data;
 			});
 		}
 	}
