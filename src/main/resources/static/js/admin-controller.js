1cad16db52114586c384f3a7e43852ec45abd4a0
royryando <royryando7@gmail.com>
2017-07-04 08:24:08 +0700

    web resource

--- src/main/resources/static/js/admin-controller.js
+++ src/main/resources/static/js/admin-controller.js
@@ -19,12 +19,13 @@
 		vm.status = 'Loading...';
 		vm.disabled = true;
 		vm.dataTipe = '';
-		vm.stok_habis = 0;
+		vm.stok_habis = 0; //unused
 		
 		vm.produk = [];
 		vm.deskripsi = [];
 		vm.cacheProduk = []; // Array produk sebelum dikirim ke API CREATE atau UPDATE
 		vm.tipe = [];
+		vm.cacheTipe = [];
 
 		vm.deskripsiProduk = deskripsiProduk;
 		vm.listProduk = listProduk;
@@ -34,14 +35,22 @@
 		vm.listTipe = listTipe;
 		vm.cariNamaTipe = cariNamaTipe;
 		vm.createProduk = createProduk;
+		vm.createupdateTipe = createupdateTipe;
+		vm.setCacheTipe = setCacheTipe;
+		vm.remCacheTipe = remCacheTipe;
+		vm.deleteTipe = deleteTipe;
 		//vm.cekStokHabis = cekStokHabis;
 		
 		init();
 		
 		function init(){
+		    listTipe();
 			listProduk();
 		}
-		
+
+		/*
+		*   Fungsi untuk Produk
+		 */
 		function listProduk(){
 			var url = urlAPI + "/produk/list";
 			var kasirPromise = $http.get(url);
@@ -58,11 +67,14 @@
 			});
 		}
 
-		function hapusProduk(kode){
-			var url = urlAPI + "/produk/delete/" + kode;
-			$http.post(url).then(function(response){
-				vm.produk = response.data;
-			});
+		function hapusProduk(kode, nama){
+		    if(confirm('Yakin ingin menghapus produk ' + nama + '?') == true){
+
+                var url = urlAPI + "/produk/delete/" + kode;
+                $http.post(url).then(function(response){
+                    vm.produk = response.data;
+                });
+            }
 		}
 
 		function setCache(kode){
@@ -77,14 +89,6 @@
 			vm.cacheProduk = [];
 		}
 
-		function listTipe(){
-			var url = urlAPI + "/tipe/list";
-			var kP = $http.get(url);
-			kP.then(function (response) {
-				vm.tipe = response.data;
-            });
-		}
-
 		function cariNamaTipe(kode){
 			var url = urlAPI + "/tipe/cari-by-kode/" + kode;
 			var prom = $http.get(url);
@@ -94,18 +98,84 @@
 		}
 
 		function createProduk(){
-			var url = urlAPI + "/produk/createupdate";
-			// TODO respon body ke URL
-			console.log(url);
+		    // validasi
+            /*
+            if(vm.cacheProduk.kode_produk == '' || vm.cacheProduk.nama == '' || vm.cacheProduk.tipe.kode_tipe == '' || vm.cacheProduk.stok =='' || vm.cacheProduk.harga_jual == '' || vm.cacheProduk.harga_beli == '' || vm.cacheProduk.deskripsi == ''){
+                alert('Data yang anda masukkan belum lengkap');
+            }else if(vm.cacheProduk.harga_jual >= 9999999999){
+                alert('Silahkan masukkan harga jual kurang dari 10 digit');
+            }else if(vm.cacheProduk.harga_beli >= 9999999999) {
+                alert('Silahkan masukkan harga jual kurang dari 10 digit')
+            }else if(vm.cacheProduk.stok >= 9999999999){
+                alert('Silahkan masukkan stok kurang dari 10 digit');
+            }else{*/
+                var url = urlAPI + "/produk/createupdate";
+                var prom = $http.post(url, {
+                    kode_produk: vm.cacheProduk.kode_produk,
+                    tipe: {
+                        kode_tipe: vm.cacheProduk.tipe.kode_tipe
+                    },
+                    nama: vm.cacheProduk.nama,
+                    stok: vm.cacheProduk.stok,
+                    harga_jual: vm.cacheProduk.harga_jual,
+                    harga_beli: vm.cacheProduk.harga_beli,
+                    tanggal_masuk: "2000-04-24",
+                    deskripsi: vm.cacheProduk.deskripsi
+                });
+                prom.then(function(response){
+                    vm.produk = response.data;
+                });
+                // TODO respon body ke URL
+                //console.log(url);
+            //} //Tutup validasi
 		}
-/*
-		function cekStokHabis(){
-		    var url = urlAPI + "/produk/stok-habis";
-		    var prom = $http.get(url);
-		    prom.then(function (response) {
-                vm.stok_habis = response.data;
+
+		/*
+		*   Fungsi untuk Tipe
+		 */
+
+        function listTipe(){
+            var url = urlAPI + "/tipe/list";
+            var kP = $http.get(url);
+            kP.then(function (response) {
+                vm.tipe = response.data;
             });
         }
-*/
+
+        function createupdateTipe(){
+            if(vm.status == 'Tambah Tipe'){
+                vm.cacheTipe.kode_tipe = 0;
+            }
+            var url = urlAPI + '/tipe/createupdate';
+            var prom = $http.post(url, {
+                kode_tipe: vm.cacheTipe.kode_tipe,
+                tipe: vm.cacheTipe.tipe
+            });
+            prom.then(function(response){
+               vm.tipe = response.data;
+            });
+        }
+
+        function setCacheTipe(kode){
+            var url = urlAPI + "/tipe/cari-by-kode/" + kode;
+            var prom = $http.get(url);
+            prom.then(function (response) {
+                vm.cacheTipe = response.data;
+            });
+        }
+
+        function remCacheTipe(){
+            vm.cacheTipe = [];
+        }
+
+        function deleteTipe(kode, nama){
+            if(confirm('Yakin ingin menghapus tipe ' + nama + ' dan\nsemua produk dengan tipe tersebut?') == true){
+                var url = urlAPI + '/tipe/delete/' + kode;
+                var prom = $http.post(url);
+                prom.then(function(response){
+                    vm.tipe = response.data;
+                });
+            }
+        }
 	}
 })();
