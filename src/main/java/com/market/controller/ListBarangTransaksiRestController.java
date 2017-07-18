00e01fed4c6f8d42e1c97ab7e7d6b4ed876ecc38
royryando <royryando7@gmail.com>
2017-07-18 10:05:23 +0700

    rest controller barang transaksi

--- src/main/java/com/market/controller/ListBarangTransaksiRestController.java
+++ src/main/java/com/market/controller/ListBarangTransaksiRestController.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/barang")
-public class ListBarangTransaksiController {
+public class ListBarangTransaksiRestController {
 
     @Autowired
     private ListBarangTransaksiRepository listRepo;
