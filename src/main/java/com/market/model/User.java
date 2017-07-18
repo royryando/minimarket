892ce6f381b4b5bb821e19c77a57de9166d6ebcc
royryando <royryando7@gmail.com>
2017-07-18 10:10:28 +0700

    model untuk login

---
+++ src/main/java/com/market/model/User.java
@@ -0,0 +1,89 @@
+package com.market.model;
+
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.NotEmpty;
+
+import javax.persistence.*;
+import java.util.Set;
+
+/**
+ * Created by Roy on 16/07/2017.
+ */
+@Entity
+@Table(name = "user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "user_id")
+    private int id;
+
+    @Column(name = "username")
+    @NotEmpty(message = "*Masukan Username")
+    private String username;
+
+    @Column(name = "password")
+    @Length(min = 5, message = "*Password Setidaknya 5 Karakter")
+    @NotEmpty(message = "*Masukan Password")
+    @Transient
+    private String password;
+
+    @Column(name = "name")
+    @NotEmpty(message = "*Masukan Nama")
+    private String name;
+
+    @Column(name = "active")
+    private int active;
+
+    @ManyToMany(cascade = CascadeType.ALL)
+    @JoinTable(name = "user_role", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getActive() {
+        return active;
+    }
+
+    public void setActive(int active) {
+        this.active = active;
+    }
+
+    public Set<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<Role> roles) {
+        this.roles = roles;
+    }
+}
