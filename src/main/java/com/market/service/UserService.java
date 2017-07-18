e0fc8c9430af429ed15ab4166964ed90fab3106b
royryando <royryando7@gmail.com>
2017-07-18 10:11:21 +0700

    user service

---
+++ src/main/java/com/market/service/UserService.java
@@ -0,0 +1,12 @@
+package com.market.service;
+
+import com.market.model.User;
+
+/**
+ * Created by Roy on 16/07/2017.
+ */
+public interface UserService {
+    public User findUserByUsername(String username);
+    public void saveUser(User user);
+
+}
