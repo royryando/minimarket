da1001d5a4f8a60c11ed8df2d0e165ed457d9c59
royryando <royryando7@gmail.com>
2017-07-20 16:09:10 +0700

    fix finished

--- src/main/java/com/market/controller/MainController.java
+++ src/main/java/com/market/controller/MainController.java
@@ -4,11 +4,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-/**
- * Created by Roy on 16/07/2017.
- */
 @Controller
 public class MainController {
+
+    @RequestMapping({"/", ""})
+    public String awal(){
+        return "awal";
+    }
+
     @RequestMapping("/login")
     public String login(){
         return "login";
