8b3a0f0cdf9c680bd504346931fc477d065e9199
RaniTrianiG <trianirani71@gmail.com>
2017-07-20 14:44:31 +0700

    -_-

--- src/main/java/com/market/service/UserService.java
+++
@@ -1,8 +0,0 @@
-package com.market.service;
-
-import com.market.model.User;
-
-public interface UserService {
-	public User findUserByUsername(String username);
-	public void saveUser(User user);
-}
